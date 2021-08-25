452b6f07b23614b3ce0c74892f93c9dc583954ae
LEOPOLDO GABRIEL <leogabriel2000@yahoo.com>
2021-08-25 15:21:14 +0800

    added spring boot

---
+++ webrest/src/test/java/com/exone/webrest/WebrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exone.webrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
